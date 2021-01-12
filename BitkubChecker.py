c9f34bd23af5baabbf4789ffb491a864c009cc9f
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2021-01-12 23:26:33 +1100

    Add files via upload

---
+++ BitkubChecker.py
@@ -0,0 +1,23 @@
+#BitkubChecker.py
+
+import requests
+from pprint import pprint
+import time
+
+API_HOST ='https://api.bitkub.com'
+
+mycoin = ['THB_BTC','THB_ETH','THB_DOGE']
+
+while True:
+    response = requests.get(API_HOST + '/api/market/ticker')
+    result = response.json()
+
+    #pprint(result)
+    for c in mycoin:
+        sym = c
+        data = result[sym]
+        last = data['last']
+        #print(data)
+        print(sym,last)
+        print('--------------')
+        time.sleep(0.2)
